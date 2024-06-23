3dfb54ff3935d78602941123101ad1325acc9843
ElvDev05 <vasquezelverth@gmail.com>
2024-06-23 18:30:41 -0500

    some changes

--- src/books/pages/book-detail.vue
+++ src/books/pages/book-detail.vue
@@ -1,6 +1,24 @@
 <script>
+import {Book} from "../../admin-books/model/book.entity.js";
+import {BooksApiService} from "../../admin-books/services/books-api.service.js";
+
 export default {
-  name: "book-detail"
+  name: "book-detail",
+  data(){
+    return {
+      books:[],
+      book:{},
+      bookService:null
+    }
+  },
+  created(){
+    this.bookService = new BooksApiService();
+    console.log(this.$route.params.bookId);
+    this.bookService.getById(this.$router.params.bookId).then((response)=>{
+      let book = response.data;
+      this.book = book.map((book)=> Book.toDisplayableBook(book));
+    })
+  }
 }
 </script>
 
