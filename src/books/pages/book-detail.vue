83822799f55ce2cbdbddf8ae2bf645b123dd76e3
ElvDev05 <vasquezelverth@gmail.com>
2024-06-23 19:26:01 -0500

    feat: implement user detail Books

--- src/books/pages/book-detail.vue
+++ src/books/pages/book-detail.vue
@@ -14,18 +14,116 @@ export default {
   created(){
     this.bookService = new BooksApiService();
     console.log(this.$route.params.bookId);
-    this.bookService.getById(this.$router.params.bookId).then((response)=>{
-      let book = response.data;
-      this.book = book.map((book)=> Book.toDisplayableBook(book));
+    this.bookService.getById(this.$route.params.bookId).then((response)=>{
+
+      this.book = response.data;
     })
   }
 }
 </script>
 
 <template>
-DETALLE DEL PRODUCTO
+<div class="content-container">
+  <div class="details-container">
+    <section class="image-section">
+      <div class="container-image">
+        <img class="img-book" src="https://th.bing.com/th/id/OIP.NFU6Gp9gB-Tn1ONrEoIcpQHaJy?w=202&h=267&c=7&r=0&o=5&dpr=1.3&pid=1.7" />
+      </div>
+      <pv-button class="btn-buy" label="Buy"></pv-button>
+    </section>
+
+
+    <section class="details-section">
+      <div style="display:flex; gap:5px">
+        <p class="subtitle">Title:</p>
+        <p>{{this.book.titulo}}</p>
+      </div>
+
+      <div style="display:flex; gap:5px">
+        <p class="subtitle">Author:</p>
+        <p>{{this.book.autor}}</p>
+      </div>
+      <div style="display:flex; gap:5px">
+        <p class="subtitle">Price:</p>
+        <p>{{this.book.precio}}</p>
+      </div>
+      <div style="display:flex; gap:5px">
+        <p class="subtitle">Category:</p>
+        <p>{{this.book.categoria}}</p>
+      </div>
+      <div style="display:flex; gap:5px">
+        <p class="subtitle">Condition:</p>
+        <p>{{this.book.condicion}}</p>
+      </div>
+      <div style="display:flex; gap:5px">
+        <p class="subtitle">Type:</p>
+        <p>{{this.book.formato}}</p>
+      </div>
+      <div>
+        <p class="subtitle">Description:</p>
+        <p>{{this.book.descripcion}}</p>
+      </div>
+    </section>
+  </div>
+
+  <div class="reviews-container">
+    <section class="reviews-section">
+    <p>
+      somos reviews
+    </p>
+    </section>
+
+  </div>
+
+
+
+</div>
+
 </template>
 
 <style scoped>
+.content-container{
+  max-width:1500px;
+  margin: 0 auto;
+  display: flex;
+  padding-top:30px;
+  flex-direction: column;
+}
+.details-container{
+  display:flex;
+  gap:55px;
+  align-items:center;
+  justify-content:space-between;
+}
 
+.details-section{
+  border:1px solid black;
+  padding:15px;
+  border-radius:15px;
+}
+.image-section{
+  height: 500px;
+  width:700px;
+  display:flex;
+  flex-direction:column;;
+  gap:10px;
+  padding:10px;
+  justify-content:center;
+  align-items:center;
+}
+.container-image{
+  height:400px;
+  width: 400px;
+}
+.img-book{
+  height: 100%;
+  width: 100%;
+}
+.btn-buy{
+  height:50px;
+  width: 90%;
+}
+.subtitle{
+  font-weight:bold;
+}
 </style>
